d05704f20f9a0bfc462eb92443968ab2e493e8eb
˜Rafael <rafaelwvs16@gmail.com>
2022-10-03 18:32:43 -0300

    App funcional

--- appBanco.py
+++ appBanco.py
@@ -1,14 +1,14 @@
 nome = input("Digite seu nome ")
 saldo = 3000
 
-Opções = int(input("Informe uma opção:[1]Saque\n [2] Extrato \n [3] Investimentos\n [4]Depositar:"))
+Opções = int(input("Informe uma opção: [1] Saque\n [2] Extrato \n [3] Investimentos\n [4] Depositar:"))
 if Opções == 1:
     valor = float(input("Informe o valor que deseja sacar "))
 
     if valor<=saldo:
-        print("saque autorizado", (nome), "Seu saldo disponivel e ", (saldo-valor))
+        print("saque autorizado",(nome), "Seu saldo disponivel e ", (saldo-valor))
     else:
-        print("Ola {name} Seu saldo e insuficiente", "saldo disponivel",(saldo))
+        print("Ola",(nome),"Seu saldo e insuficiente", "saldo disponivel",(saldo))
 
 if Opções == 2:
     print (saldo)
@@ -20,6 +20,6 @@ if Opções == 4:
     deposito = float(input("valor do deposito "))
 
     if deposito >= 0:
-        print(" Seu deposito foi realizado, seu novo saldo e de "(saldo+deposito))
+        print(" Seu deposito foi realizado, seu novo saldo e de ",(saldo+deposito))
     else:
         print("valor insuficiente")
